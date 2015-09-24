52b5c2b1f9a99e5ded830be5afc6b7410620fb4e
Chris Lemke <sonicelectronik314@gmail.com>
2015-09-24 10:51:22 -0500

    Initial Commit

---
+++ DontHurtMyEyes-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DontHurtMyEyes-iOS
+//
+//  Created by Chris Lemke on 9/24/15.
+//  Copyright © 2015 Chris Lemke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
